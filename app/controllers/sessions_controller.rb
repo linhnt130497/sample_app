40cf64956c56324170321231298bb25588ab72cb
Nguyen Linh <linhnt130497@gmail.com>
2017-10-26 13:41:14 +0700

    advance login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by email: params[:session][:email].downcase
-
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
       redirect_to user
     else
       flash[:danger] = t "controllers.sessions_controller.notify_invalid"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,36 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save {self.email.downcase!}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
   validates :name, presence: true, length: {maximum: Settings.user.max_length_name}
   validates :password, presence: true, length: {minimum: Settings.user.min_length_password}
   validates :email, presence: true, length: {maximum: Settings.user.max_length_email},
-  	format: {with: VALID_EMAIL_REGEX},
-  	uniqueness: {case_sensitive: false}
+    format: {with: VALID_EMAIL_REGEX},
+      uniqueness: {case_sensitive: false}
   has_secure_password
   scope :select_name_email, ->{select :name, :email}
   scope :order_by_date, ->{order created_at: :asc}
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost) 
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+      update_attributes :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attributes :remember_digest, nil
+  end
 end 
