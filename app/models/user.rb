8100adacb1c8bc7d047ef5f6129fe5a85f732348
Nguyen Linh <linhnt130497@gmail.com>
2017-11-09 08:32:56 +0700

    updating-user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
   before_save {self.email.downcase!}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: {maximum: Settings.user.max_length_name}
-  validates :password, presence: true, length: {minimum: Settings.user.min_length_password}
+  validates :password, presence: true, length: {minimum: Settings.user.min_length_password}, allow_nil: true
   validates :email, presence: true, length: {maximum: Settings.user.max_length_email},
     format: {with: VALID_EMAIL_REGEX},
       uniqueness: {case_sensitive: false}
   has_secure_password
-  scope :select_name_email, ->{select :name, :email}
+  scope :select_name_email, ->{select :id, :name, :email}
   scope :order_by_date, ->{order created_at: :asc}
 
   def User.digest(string)
@@ -31,6 +31,6 @@ class User < ApplicationRecord
   end
 
   def forget
-    update_attributes :remember_digest, nil
+    update_attributes remember_digest: nil
   end
 end 
